1b2dd5a3e2d566e17f04b52bebefffaeffde559e
bruno4224onurb <brunos.franchi@gmail.com>
2024-02-22 07:49:58 -0300

    tests with get and post using json

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,14 @@ const app = express()
 //the same as saying create server
 
 //app.httpmethod(Path, Handler)
+//get example
 app.get('/', (req,res)=>{
-    res.send('Hello User')
+    res.status(200).json({msg: "Hello User"})
+})
+
+//post example
+app.post('/', (req,res)=>{
+    res.status(200).json({msg: "Post Request"})
 })
 
 app.listen(4000, 'localhost', ()=>console.log("Listening to port 4000."))
