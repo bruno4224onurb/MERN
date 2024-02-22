602b953ef7b416cfcbde0665dd89ee26243c5bdc
bruno4224onurb <brunos.franchi@gmail.com>
2024-02-22 07:35:05 -0300

    expanded the tests with requests and responses into the server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
 import http from 'http'
 
 //creating server to accept requests and responses
-http.createServer((req, res) => {
+http.createServer( (req, res) => {
+//res.writeHead(status code, 'status message', {Headers})
+    res.writeHead(400, 'Bad request', {"Content-type":"text/html"})
+
+    res.write(req.url)
+
+// if the request is post write post request
+    if (req.method === "POST") {
+        res.write('Post Request')
+    }
+
+    res.end()
+//must end the response
 
 }).listen(4000, 'localhost')
