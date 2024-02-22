e75c1631310ec028fb879d72705ca2b1724dfff7
bruno4224onurb <brunos.franchi@gmail.com>
2024-02-22 07:45:31 -0300

    First tests with express.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,11 @@
-import http from 'http'
+import express from 'express'
 
-//creating server to accept requests and responses
-http.createServer( (req, res) => {
-//res.writeHead(status code, 'status message', {Headers})
-    res.writeHead(400, 'Bad request', {"Content-type":"text/html"})
+const app = express()
+//the same as saying create server
 
-    res.write(req.url)
+//app.httpmethod(Path, Handler)
+app.get('/', (req,res)=>{
+    res.send('Hello User')
+})
 
-// if the request is post write post request
-    if (req.method === "POST") {
-        res.write('Post Request')
-    }
-
-    res.end()
-//must end the response
-
-}).listen(4000, 'localhost')
+app.listen(4000, 'localhost', ()=>console.log("Listening to port 4000."))
